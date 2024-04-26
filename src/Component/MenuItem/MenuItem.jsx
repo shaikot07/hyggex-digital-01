7796ecef58e80d3cee2f7c2938f0180310f74dc8
shaikot07 <saifulislamshaikat007@gmail.com>
2024-04-26 23:57:09 +0600

    adde active menu item

--- src/Component/MenuItem/MenuItem.jsx
+++ src/Component/MenuItem/MenuItem.jsx
@@ -1,10 +1,14 @@
+import { CiHome } from "react-icons/ci";
+import {  FaChevronRight } from "react-icons/fa";
 
 
 const MenuItem = () => {
     return (
-        <div>
-            <ul>
-                <li> Flashcard</li>
+        <div className="mt-10">
+            <ul className="menu-horizontal px-1">
+                <li className="flex items-center text-[#696671] text-1xl font-semibold "><CiHome className="text-2xl"/><FaChevronRight className="text-[#06286E]"/> Flashcard</li>
+                <li className="flex items-center text-[#696671] text-1xl font-semibold "><FaChevronRight className="text-[#06286E]"/> Mathematics</li>
+                <li className="flex items-center text-[#06286E] text-1xl font-semibold "><FaChevronRight className="text-[#06286E]"/> Relation and Function</li>
             </ul>
         </div>
     );
--- src/Home/NavBar/NavBar.jsx
+++ src/Home/NavBar/NavBar.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import MenuItem from "../../Component/MenuItem/MenuItem";
 
 const NavBar = () => {
     const link = <>
@@ -8,26 +9,33 @@ const NavBar = () => {
         <li> <Link className="rounded-none text-black font-bold border-b-0 hover:!text-[#CC2229] hover:border-[#CC2229] hover:border-b-2 transition duration-300 focus:!text-white" to={'/'}>FAQ</Link> </li>
     </>
     return (
-        <div className="navbar bg-[#FFFFFF] max-w-6xl mx-auto">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        <div className="max-w-6xl mx-auto">
+            <div className="navbar bg-[#FFFFFF] ">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </div>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            {link}
+                        </ul>
                     </div>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <div>
+                        <img className="w-[150px] h-[39px]" src="https://i.postimg.cc/sx8qM7PW/Component-40.png" alt="" />
+                    </div>
+                    {/* <a className="btn btn-ghost text-xl">LOGO</a> */}
+                </div>
+                <div className="navbar-center  hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
                         {link}
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">LOGO</a>
+                <div className="navbar-end w-[110px]">
+                    <Link className="py-2 px-8 bg-gradient-to-b from-blue-900 to-blue-600 text-white rounded-3xl">Login</Link>
+                </div>
+
             </div>
-            <div className="navbar-center  hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                {link}
-                </ul>
-            </div>
-            <div className="navbar-end w-[110px]">
-                <Link className="py-2 px-8 bg-gradient-to-b from-blue-900 to-blue-600 text-white rounded-3xl">Login</Link>
-            </div>
+            <MenuItem></MenuItem>
         </div>
     );
 };
