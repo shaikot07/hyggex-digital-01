5aa414a090817df66c61db43e62d54629eb05d5b
shaikot07 <saifulislamshaikat007@gmail.com>
2024-04-28 00:10:29 +0600

    added all tab panel items

--- src/Home/TabSection/StudyTab/StudyTab.jsx
+++ src/Home/TabSection/StudyTab/StudyTab.jsx
@@ -1,9 +1,13 @@
 
 
-const StudyTab = () => {
+const StudyTab = ({src, type}) => {
     return (
-        <div>
-            
+        <div className="w-[500px] h-[300px] rounded-3xl bg-gradient-to-b from-blue-900 to-blue-600 ">
+            {/* <video src=""></video> */}
+            <video controls className="w-[500px] h-[300px] rounded-3xl">
+                <source src={src} type={type} />
+               
+            </video>
         </div>
     );
 };
--- src/Home/TabSection/TabSection.jsx
+++ src/Home/TabSection/TabSection.jsx
@@ -1,20 +1,18 @@
 import { useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import StudyTab from './StudyTab/StudyTab';
 
 const TabSection = () => {
-    const categories = ['salad', 'pizza', 'soup', 'dessert', 'drinks']
-    const category = []
-    const initialIndex = categories.indexOf(category)
-    // console.log(initialIndex);
-    // const [tabIndex, setTabIndex] = useState(initialIndex);
+    const categories = ['salad', 'pizza', 'soup', 'dessert', 'drinks'];
     const [tabIndex, setTabIndex] = useState(0);
+
     return (
         <div className='max-w-6xl mx-auto'>
             <h2 className='text-[32px] font-bold bg-clip-text text-transparent bg-gradient-to-b from-blue-900 to-blue-500'>Relations and Functions ( Mathematics )</h2>
-            <div className='bg-slate-400 w-[500px] h-[389px] mx-auto mt-16'>
+            <div className=' w-[500px] h-[389px] mx-auto mt-16'>
                 <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
-                    <TabList>
+                    <TabList className="mb-8 bg-slate-500 pb-2 w-[390px] mx-auto text-[#696671] text-base font-semibold">
                         <Tab>Study</Tab>
                         <Tab>Quiz</Tab>
                         <Tab>Test</Tab>
@@ -22,27 +20,46 @@ const TabSection = () => {
                         <Tab>Others</Tab>
                     </TabList>
                     <TabPanel>
-                        {/* <OrderTab items={salad}></OrderTab> */}
-                        <h1>hello this is study </h1>
+                        <StudyTab src={""} type={''}></StudyTab>
                     </TabPanel>
                     <TabPanel>
-                        {/* <OrderTab items={pizza}></OrderTab> */}
-                        <h1>hello this is Quiz</h1>
+                        <div className='w-[500px] h-[300px] rounded-3xl bg-gradient-to-b from-blue-900 to-blue-600'>
+                            <h1 className='text-2xl text-white font-semibold text-center'>hello this is Quiz</h1>
+                        </div>
                     </TabPanel>
                     <TabPanel>
-                        {/* <OrderTab items={soup}></OrderTab> */}
-                        <h1>hello this is test </h1>
+                        <div className='w-[500px] h-[300px] rounded-3xl bg-gradient-to-b from-blue-900 to-blue-600'>
+                            <h1 className='text-2xl text-white font-semibold text-center'>hello this is test</h1>
+                        </div>
+
                     </TabPanel>
                     <TabPanel>
-                        {/* <OrderTab items={desserts}></OrderTab> */}
-                        <h1>hello this is game </h1>
+                        <div className='w-[500px] h-[300px] rounded-3xl bg-gradient-to-b from-blue-900 to-blue-600'>
+                            <h1 className='text-2xl text-white font-semibold text-center'>hello this is game</h1>
+                        </div>
+
                     </TabPanel>
                     <TabPanel>
-                        {/* <OrderTab items={drinks}></OrderTab> */}
-                        <h1>hello this is other </h1>
+                        <div className='w-[500px] h-[300px] rounded-3xl bg-gradient-to-b from-blue-900 to-blue-600'>
+                            <h1 className='text-2xl text-white font-semibold text-center'>hello this is other</h1>
+                        </div>
+
                     </TabPanel>
                 </Tabs>
             </div>
+
+            {/* Add the following style within your component */}
+            <style>
+                {`
+                .react-tabs__tab--selected {
+                    border: 4px solid #06286E;
+                  
+                }
+                .react-tabs__tab {
+                    border-bottom: -10px;
+                }
+              `}
+            </style>
         </div>
     );
 };
